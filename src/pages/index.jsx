0b4b695c7cb921cfe8b2073f3c38f07448e38196
Casey G <15313423+CJGjr@users.noreply.github.com>
2022-12-22 11:12:33 +0000

    reset to barebones template

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const IndexPage = () => (
+    <>
+        <h1 className="text-center text-green-600">This will eventually be a website!</h1>
+
+    </>
+)
+
+export default IndexPage;
